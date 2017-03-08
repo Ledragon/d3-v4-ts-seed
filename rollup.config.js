fd163e9af4d234286ad10a1fe9264d1431c6843d
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-03-08 21:15:20 +0100

    Working config

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,19 @@
+import resolve from 'rollup-plugin-node-resolve';
+
 export default {
     entry: 'src/app.js',
     dest: 'src/bundle.js',
     format: 'umd',
+    // globals: { d3: 'd3' },
+    // external: ['d3'],
     plugins: [
         // scss(),
-        // resolve({
-        //     jsnext: true,
-        //     browser: true,
-        //     main: true,
-        //     module: true,
-        //     skip: d3Array
-        // })
+        resolve({
+            jsnext: true,
+            browser: true,
+            main: true,
+            module: true
+        })
     ],
     moduleName: 'app'
 };
