28198950b2635557aba91aa198cc0db1b07c1f4a
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-03-09 19:49:56 +0100

    Module resolution

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,10 @@ export default {
     entry: 'src/app.js',
     dest: 'src/bundle.js',
     format: 'umd',
-    // globals: { d3: 'd3' },
-    // external: ['d3'],
     plugins: [
-        // scss(),
         resolve({
             jsnext: true,
-            browser: true,
+            // browser: true,
             main: true,
             module: true
         })
